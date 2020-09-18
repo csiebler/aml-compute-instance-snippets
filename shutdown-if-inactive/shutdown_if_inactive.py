749c4e7e823dc0667a4fb7dae8fb8af61e01b2d8
Clemens Siebler <clemens.siebler@gmail.com>
2020-09-18 13:34:06 +0200

    Minor refactoring

--- shutdown-if-inactive/shutdown_if_inactive.py
+++ shutdown-if-inactive/shutdown_if_inactive.py
@@ -35,19 +35,23 @@ def get_notebook_sessions():
         })
     return notebooks
 
-def is_notebook_busy(notebook):
+def was_notebook_recently_updated(notebook):
     last_activity = datetime.strptime(notebook['last_activity'],"%Y-%m-%dT%H:%M:%S.%fz")
     return (datetime.now() - last_activity).total_seconds() < idle_threshold_in_sec
 
-def is_instance_idle(notebooks):
+def are_all_notebooks_idle(notebooks):
     for n in notebooks:
+        print('Notebook:',  n['path'])
+        print('Notebook state:',  n['state'])
+        print('Notebook connections:',  n['num_connections'])
+        print('Was notebook recently updated:', was_notebook_recently_updated(n))
         if (n['state'] != 'idle' or
             n['num_connections'] > 0 or
-            is_notebook_busy(n)):
+            was_notebook_recently_updated(n)):
             return False
     return True
 
-def get_uptime():
+def get_instance_uptime():
     with open('/proc/uptime', 'r') as f:
         uptime_seconds = float(f.readline().split()[0])
     return uptime_seconds
@@ -55,14 +59,13 @@ def get_uptime():
 notebooks = get_notebook_sessions()
 ci_name = get_compute_instance_name()
 
-if is_instance_idle(notebooks) and get_uptime() > idle_threshold_in_sec:
-    # Get name of current Compute Instance
+if are_all_notebooks_idle(notebooks) and get_instance_uptime() > idle_threshold_in_sec:
     print(f'Compute Instance {ci_name} will be shut down!')
     # Connect to workspace and stop instance
     ws = Workspace.from_config()
     ct = ComputeInstance(ws, ci_name)
     ct.stop(wait_for_completion=False, show_output=False)
 else:
-    print(f'Compute Instance {ci_name} still has notebooks running, will not shut it down')
+    print(f'Compute Instance {ci_name} still has notebooks running or just started, will not shut it down')
     print("Details:")
     print(json.dumps(notebooks, indent=2))
