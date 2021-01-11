075efd453c313b2bb7808191e173dbd956e11dca
Clemens Siebler <clemens.siebler@gmail.com>
2021-01-11 17:23:54 +0100

    Updated setup script

--- shutdown-if-inactive/setup_cron.sh
+++ shutdown-if-inactive/setup_cron.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+echo "Setting up cronjob for instance auto-shutdown"
 
-set -e
-(crontab -l 2>/dev/null; echo "*/5 * * * * python $PWD/shutdown_if_inactive.py") | crontab -
+# Get path to script
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+echo "Current working dir: $PWD"
+echo "Path to scripts: $SCRIPT_PATH"
+
+# Get python path
+PYTHON_PATH=`which python`
+echo "Python Path: $PYTHON_PATH"
+
+# Add cron to check every 5min if instance is inactive
+echo "Current crontab:"
+crontab -l
+
+echo "Adding crontab entry for instance auto-shutdown"
+(crontab -l 2>/dev/null; echo "*/1 * * * * $PYTHON_PATH $SCRIPT_PATH/shutdown_if_inactive.py > /tmp/shutdown.log 2>&1") | crontab -
+
+echo "New crontab:"
+crontab -l
